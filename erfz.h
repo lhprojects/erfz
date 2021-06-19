a52cf87ffd95970adb72ece3e0a04e4b8011cec3
Harry <lihawl@hotmail.com>
2021-06-20 03:48:13 +0800

    refine impl

--- erfz.h
+++ erfz.h
@@ -4,10 +4,17 @@
 #include <cmath>
 #include <math.h>
 
+double sinc(double x)
+{
+	if (x < 1E-10) return 1.;
+	else return sin(x) / x;
+}
+
 // This implementation is following:
 // https://granite.phys.s.u-tokyo.ac.jp/svn/LCGT/trunk/sensitivity/Matlab/official/@double/erfz.pdf
 inline std::complex<double> erfz(std::complex<double> const &c)
 {
+	typedef std::complex<double> Complex;
 	double const Pi = 3.141592653589793238462643383279502884197169399375;
 	double const Re = c.real();
 	double const R2 = Re * Re;
@@ -20,39 +27,42 @@ inline std::complex<double> erfz(std::complex<double> const &c)
 
 	double erfR = erf(Re);
 
-	double E_R = exp(-R2) / (2 * Pi * Re) * (1 - cos(-2 * Re * Im));
-	double E_I = exp(-R2) / (2 * Pi * Re) * (0 - sin(-2 * Re * Im));
+    Complex E_ = exp(-R2) / (2 * Pi) * 2 * Im *
+        Complex(sin(Re * Im) * sinc(Re * Im), sinc(2 * Re * Im));
 
-	int const N = 15;
-	double F_R = 0;
+
+    // 	exp(-0.25 * 12 ^ 2) / (0.25 * 12 ^ 2) = 6E-18
+    int const N = 15;
+    double F_R = 0;
+    for (int n = N; n >= 1; --n) {
+        F_R += exp(-0.25 * n * n) / (0.25 * n * n + R2);
+    }
+    F_R *= exp(-R2) * Re / Pi;
+
+
+	int const M = (int)(2 * Im);
+	int const M_N = M - N > 1 ? M - N : 1;
+
+	Complex H(0., 0.);
+	Complex G(0., 0.);
+
 	for (int n = N; n >= 1; --n) {
-		F_R += exp(-0.25*n*n) / (0.25*n*n + R2);
-	}
-	F_R *= exp(-R2) * Re / Pi;
+        H += exp(-0.25 * n * n - n * Im - R2) / (0.25 * n * n + R2) * Complex(Re, 0.5 * n);
+    }
 
-	int M = (int)(2 * Im);
-	double H_R = 0;
-	double H_I = 0;
-	double G_R = 0;
-	double G_I = 0;
-	int M_N = M - N > 1 ? M - N : 1;
 	for (int n = M + N; n >= M_N; --n) {
-		H_R += exp(-0.25*n*n - n * Im - R2) / (0.25*n*n + R2) * Re;
-		H_I += exp(-0.25*n*n - n * Im - R2) / (0.25*n*n + R2) * (0.5 * n);
-
-		G_R += exp(-0.25*(n - 2 * Im)*(n - 2 * Im) + (Im*Im - R2)) / (0.25*n*n + R2) * Re;
-		G_I += exp(-0.25*(n - 2 * Im)*(n - 2 * Im) + (Im*Im - R2)) / (0.25*n*n + R2) * (-0.5 * n);
+        //G += exp(-0.25 * (n - 2 * Im) * (n - 2 * Im) + (Im * Im - R2)) / (0.25 * n * n + R2) * Complex(Re, -0.5 * n);
+		G += exp(-0.25 * n * n + n * Im - R2) / (0.25 * n * n + R2) * Complex(Re, -0.5 * n);
 	}
-	H_R *= 1 / (2 * Pi);
-	H_I *= 1 / (2 * Pi);
-	G_R *= 1 / (2 * Pi);
-	G_I *= 1 / (2 * Pi);
 
-	std::complex<double> a(cos(-2 * Re * Im), sin(-2 * Re * Im));
-	std::complex<double> b = a * std::complex<double>(G_R + H_R, G_I + H_I);
-	double real = erfR + E_R + F_R - b.real();
-	double imag = 0 + E_I + 0 - b.imag();
+	H *= 1. / (2 * Pi);
+	G *= 1. / (2 * Pi);
 
-	if (conj) return std::complex<double>(real, -imag);
-	return std::complex<double>(real, imag);
+	Complex a(cos(-2 * Re * Im), sin(-2 * Re * Im));
+	Complex b = a * (G + H);
+	double real = erfR + E_.real() + F_R - b.real();
+	double imag = 0 + E_.imag() + 0 - b.imag();
+
+	if (conj) return Complex(real, -imag);
+	return Complex(real, imag);
 }
